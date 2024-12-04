a2e8368efd23a9d5f2b8ead6623cbacb4199c46c
Miraiyy <edhankhun69@gmail.com>
2024-12-04 21:08:27 +0100

    Day4 part 2

--- DAy4.py
+++ DAy4.py
@@ -12,11 +12,12 @@ for row in grid:
     print(row)
 
 # grid[y][x]
-# x = 5
-# y = 1
-# elem = grid[1][5]
+# x = 4
+# y = 3
 # height = len(grid)
 # width = len(grid[0])
+# elem = grid[1][5]
+
 # right_word = [grid[y][i] for i in range(x, min(x+4, width))]
 # down_right_word = [grid[y+i][x+i] for i in range(4) if y+i < height and x+i < width]
 # print(down_right_word)
@@ -63,9 +64,26 @@ def xmas_lookup(elem_coordonate: tuple, grid) -> int:
             nb_find += 1
     
     return nb_find
+
+def x_mas_lookup(elem_coordonate: tuple, grid) -> bool:
+    y, x = elem_coordonate
+    height = len(grid)
+    width = len(grid[0])
+    valid_word = [['M','A','S'],['S','A','M']]
     
+    diag_lu_rd = [grid[y + i][x + i] for i in range(-1,2) if (y + i < height and x + i < width)]
+    diag_ld_ru = [grid[y - i][x + i]for i in range(-1,2)if (y - i >= 0 and x + i < width)]
+    
+    is_diag1_valid = True if diag_lu_rd in valid_word else False
+    is_diag2_valid = True if diag_ld_ru in valid_word else False
+    
+    if is_diag1_valid and is_diag2_valid:
+        return True
+    else:
+        return False
 
 nb_xmas = 0
+nb_x_mas = 0
 for y, line in enumerate(grid):
     for x, elem in enumerate(line):
         nb_find = 0
@@ -74,7 +92,11 @@ for y, line in enumerate(grid):
         else:
             if elem == 'X':
                 nb_find = xmas_lookup((y, x), grid)
+            if elem == 'A':
+                if x_mas_lookup((y, x), grid):
+                    nb_x_mas += 1
         
         nb_xmas += nb_find
         
 print(nb_xmas)
+print(nb_x_mas)
