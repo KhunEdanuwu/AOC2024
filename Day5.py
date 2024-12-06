e504966aea2f4153cd1c8846b2f30d570181ec11
Miraiyy <edhankhun69@gmail.com>
2024-12-06 18:02:40 +0100

    Day 5 part 2 -- 1 étoiles seulement

--- Day5.py
+++ Day5.py
@@ -17,7 +17,7 @@ for i in lines:
 
 # print(rules_to_map)
 # print("----------------------")
-print(updates)
+# print(updates)
 # tt = [[int(x) for x in i.split('|')] for i in lines]
 # init mapping (key = int, value = list of value that must come before key)
 map = {}
@@ -29,14 +29,10 @@ for i in rules_to_map:
 # print(lines[0])
 # print("---------")
 # print(tt)
-print("---------")
+# print("---------")
 print(map)
-
-def dfs():
-    pass
-
       
-        
+# PArt 1    
 def is_update_order_correct(update: list[int]):
     # Check pour chaque elem si les autres elems sont mappé en tant que "doit venir avant" 
     # Si présent et index > à elem en cours alors False si aucun ne amtch cette condition ils sont bien rangés
@@ -47,8 +43,6 @@ def is_update_order_correct(update: list[int]):
                     if update.index(x) > index:
                         return False
     return True
-
-#     return True
 
 tt = 0
 for i in updates:
@@ -61,3 +55,53 @@ for i in updates:
     print(f"[{i}] -- {res}")
     
 print(tt)
+
+#Part 2
+# Si jamais je reviens dessus:
+# On passe sur les elem de l'update pour chaque on check si il y a une règle et 
+# si une n'est pas appliqué 
+# => On enregistre la pos de l'elem et de l'elem qui respecte pas la règle
+
+# En suite on les swap
+#Marche avec l'input de base
+new_upadtes = []
+for i in updates:
+    res = is_update_order_correct(i)
+    if not res:
+        incorect_ordered_update = []
+        for index, elem in enumerate(i):
+            if elem in map.keys():
+                for x in map[elem]:
+                    if x in i:
+                        if i.index(x) > index:
+                            incorect_ordered_update.append((i.index(x), index))
+        
+        for x in incorect_ordered_update:
+            i[x[0]], i[x[1]] = i[x[1]], i[x[0]]
+        new_upadtes.append(i)
+
+print("-----")
+print(new_upadtes)
+tt = 0
+for i in new_upadtes:
+    middle_index = int((len(i))/2)
+    middle_num = i[middle_index]
+    tt += middle_num
+    
+print(tt)
+
+# 97,75,47,61,53 || 61,29,13 || 97,75,47,29,13
+
+# The fourth update, 75,97,47,61,53, is not in the correct order: 
+# it would print 75 before 97, which violates the rule 97|75.
+
+# 61,13,29 =>  29|13.   
+# => 1: 29, 61, 13
+# => 2: 61, 29, 13
+
+# for i in updates:
+#     res = is_update_order_correct(i)
+#     if not res:
+#         reorder_update = reorder_update(i)
+
+# Marche po good answer 4507 ! No double star
